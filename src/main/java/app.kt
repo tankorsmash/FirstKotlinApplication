465bf7a476b0c6b37b6515459d7454a22f4e7021
tankorsmash <tankorsmash@gmail.com>
2017-12-09 00:17:04 -0500

    JSON support
    learning at least

--- src/main/java/app.kt
+++ src/main/java/app.kt
@@ -3,18 +3,32 @@ import org.http4k.core.Method
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
-import org.http4k.server.Jetty
-import org.http4k.server.asServer
+
+import org.http4k.format.Jackson
+import org.http4k.format.Jackson.asJsonArray
+import org.http4k.format.Jackson.asJsonObject
+import org.http4k.format.Jackson.asJsonValue
+import org.http4k.format.Jackson.asPrettyJsonString
+import org.http4k.format.Jackson.json
 
 
 fun main(args: Array<String>) {
     println ("poopsdsds chute")
 
-    val request = Request(Method.GET, "http://buildupthebase.com")
+    val request = Request(Method.GET, "https://www.reddit.com/user/tankorsmash.json")
     val client = ApacheClient()
 
     val response: Response = client(request)
+    val json_data = response.body.toString().asJsonObject()
 
-    println("the length of response is ${response.body.length}")
+//    val json_data =
+//            listOf(
+//            "string key" to "string val".asJsonValue(),
+//            "123" to 123.asJsonValue()
+//    ).asJsonObject()
+
+//    println(json_data["string key"])
+
+    println("the length of response is ${json_data.asPrettyJsonString()}")
 
 }
